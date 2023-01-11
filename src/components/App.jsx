710ac965d5b67633e7bd7b6fd4e7265e10de36e3
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2023-01-11 21:53:03 +0200

    started making routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,33 @@
-import { ContactForm } from './ContactForm/ContactForm';
-import { Contactlist } from './ContactList/ContactList';
-import { Filter } from './Filter/Filter';
-import { useSelector, useDispatch } from 'react-redux';
+// import { ContactForm } from './ContactForm/ContactForm';
+// import { Contactlist } from '../pages/ContactList/ContactList';
+// import { Filter } from './Filter/Filter';
+// import { useSelector, useDispatch } from 'react-redux';
 import { GlobalStyle } from './GlobalStyle';
-import { useEffect } from 'react';
-import { fetchContacts } from 'Redux/operations';
-import { Loader } from './Loader/Loader';
+// import { useEffect } from 'react';
+// import { fetchContacts } from 'Redux/operations';
+// import { Loader } from './Loader/Loader';
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from './Layout/Layout';
+import { Home } from 'pages/Home/Home';
 
 export const App = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
-  const { isLoading, error, items } = useSelector(state => state.contacts);
+  // const dispatch = useDispatch();
+  // useEffect(() => {
+  //   dispatch(fetchContacts());
+  // }, [dispatch]);
+  // const { isLoading, error, items } = useSelector(state => state.contacts);
 
   return (
-    <div>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          {/* <ContactForm /> */}
+          {/* <Filter /> */}
+          {/* <Contactlist /> */}
+        </Route>
+      </Routes>
       <GlobalStyle />
-      <h1>PhoneBook</h1>
-      <ContactForm />
-      {error ? (
-        <h2>{error}</h2>
-      ) : (
-        <>
-          {' '}
-          <h2>Contacts : {isLoading ? <Loader /> : items.length} </h2>
-          {items.length === 0 && !isLoading ? (
-            <h2>You have no contacts saved</h2>
-          ) : (
-            <>
-              <Filter />
-              <Contactlist />
-            </>
-          )}
-        </>
-      )}
-    </div>
+    </>
   );
 };
---
+++ src/components/AppBar/AppBar.jsx
@@ -0,0 +1,3 @@
+export const AppBar = () => {
+  return <h2>AppBar</h2>;
+};
