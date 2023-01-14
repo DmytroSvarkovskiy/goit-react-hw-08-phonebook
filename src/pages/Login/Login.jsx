0b5a47d8d9247ca4be0e5fd8d83c9dcf3ccb7600
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2023-01-14 11:54:33 +0200

    added form login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,3 +1,61 @@
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Checkbox, Form, Input } from 'antd';
+import { FormWrap, LogButton } from './Login.styled';
+
+import { Link } from 'react-router-dom';
 export const Login = () => {
-  return <h2>Login</h2>;
+  const onFinish = values => {
+    console.log('Received values of form: ', values);
+  };
+  return (
+    <FormWrap
+      name="normal_login"
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+    >
+      <Form.Item
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your Username!',
+          },
+        ]}
+      >
+        <Input
+          prefix={<UserOutlined className="site-form-item-icon" />}
+          placeholder="Username"
+        />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your Password!',
+          },
+        ]}
+      >
+        <Input
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="Password"
+        />
+      </Form.Item>
+      <Form.Item>
+        <Form.Item name="remember" valuePropName="checked" noStyle>
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+      </Form.Item>
+
+      <Form.Item>
+        <LogButton type="primary" htmlType="submit">
+          Log in
+        </LogButton>
+        Or <Link to="/register">register now</Link>
+      </Form.Item>
+    </FormWrap>
+  );
 };
--- src/pages/Login/Login.styled.jsx
+++ src/pages/Login/Login.styled.jsx
@@ -1,1 +1,8 @@
-
+import styled from 'styled-components';
+import { Button, Checkbox, Form, Input } from 'antd';
+export const FormWrap = styled(Form)`
+  max-width: 300px;
+`;
+export const LogButton = styled(Button)`
+  width: 100%;
+`;
