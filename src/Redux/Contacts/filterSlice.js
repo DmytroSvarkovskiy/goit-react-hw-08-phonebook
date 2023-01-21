5bd17a7a5d50b168dfb16f7fe3e5096b3c0b8897
Дмитро Сварковський <100209541+DmytroSvarkovskiy@users.noreply.github.com>
2023-01-21 22:30:32 +0200

    added filter clearing after log out

--- src/Redux/Contacts/filterSlice.js
+++ src/Redux/Contacts/filterSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
-
+import { logOut } from 'Redux/Authorization/operations';
 export const filterSlice = createSlice({
   name: 'filter',
   initialState: 
@@ -9,7 +9,11 @@ export const filterSlice = createSlice({
     filtration: (state, action) => {
       return state = action.payload;
     },
-  },
+  }, extraReducers: 
+    (builder)=>{builder.addCase(logOut.fulfilled, state => {
+     return state=''
+  })}
+  
 })
 export const { filtration } = filterSlice.actions;
 export const filterReducer = filterSlice.reducer;
